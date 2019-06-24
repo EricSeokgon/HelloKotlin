ff581dd011bf7fb841eba955fd0490a5a82d7b71
hadesyi <leesk55@gmail.com>
2019-06-25 08:42:57 +0900

    스마트 캐스트

--- src/chap02/section3/ValueRefCompare.kt
+++ src/chap02/section3/ValueRefCompare.kt
@@ -8,10 +8,7 @@ fun main() {
     val c: Int? = a
     val d: Int? = a
     val e: Int? = c
-
     println(c == d) //값의 내용만 비교하는 경우 동일하므로 true
     println(c === d) //값의 내용은 같지만 참조 주소를 비교해 다른 객체(주소 다름)이므로 false
     println(c === e) //값의 내용도 같고 참조된 객체도 동일(주소 동일)하므로 true
-
-
 }
