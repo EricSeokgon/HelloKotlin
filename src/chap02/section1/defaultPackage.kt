8e41c9818f4a56ae6739230bf780c5cab389f3ce
hadesyi <leesk55@gmail.com>
2019-06-25 08:26:50 +0900

    //기본 패키지 사용하기

--- src/chap02/section1/defaultPackage.kt
+++ src/chap02/section1/defaultPackage.kt
@@ -1,8 +1,15 @@
 package chap02.section1
-//기본 패키지 사용하기
+
+import kotlin.math.PI
+import kotlin.math.abs
+
+//수학 라이브러리 추가하기
 fun main() {
     val intro: String = "안녕하세요!"
     val num: Int = 20
+
+    println(PI)
+    println(abs(-12.6))
 
     println("intro: $intro, num:$num")
 }
