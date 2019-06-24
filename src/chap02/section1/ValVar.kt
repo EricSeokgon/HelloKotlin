e344f447bfa93cfe8b51e14fb026bbf7099275b0
hadesyi <leesk55@gmail.com>
2019-06-25 03:26:58 +0900

    val과 var변수 선언 및 할당하기

---
+++ src/chap02/section1/ValVar.kt
@@ -0,0 +1,11 @@
+package chap02.section1
+
+fun main() {
+    val number = 10 //number 변수는 Int형으로 추론
+    var language = "Korean" //language 변수는 String으로 추촌
+    val secondNumber: Int = 20 // secondNumber 변수는 자료형을 Int형으로 명시적으로 지정
+    language = "English" //var 키워드로 선언한 변수는 값을 다시 할당할수 있음
+
+    println("number: $number")
+
+}
