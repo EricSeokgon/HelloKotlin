a573bd5c0ee9263619c31406472bb9c9ce3721a0
hadesyi <leesk55@gmail.com>
2019-06-25 12:55:47 +0900

    sum() 함수 선언하고 호출하기

--- src/chap03/section1/sumFunc.kt
+++ src/chap03/section1/sumFunc.kt
@@ -4,3 +4,12 @@ fun sum(a: Int, b: Int): Int {
     var sum = a + b
     return sum
 }
+
+fun main() {
+    val result1 = sum(3, 2)
+    val result2 = sum(6, 7)
+
+    println(result1)
+    println(result2)
+
+}
