436fee7aacbfd7d5afcec95d5bac2a15435d6e57
hadesyi <leesk55@gmail.com>
2019-06-27 00:55:20 +0900

    인자와 반환값이 없는 람다식의 선언

---
+++ src/chap03/section3/HighOrderTest3.kt
@@ -0,0 +1,10 @@
+package chap03.section3
+
+fun main() {
+    val out:()->Unit={ println("Hello World!")} //인자와 반환값이 없는 람다식의 선언
+    //자료형 추론이 가능하므로 val out:()->Unit={ println("Hello World!")}와 같이 생략 가능
+
+    out() //함수처럼 사용 가능
+    val new = out //람다식이 들어 있는 변수를 다른 변수에 할당
+    new()
+}
