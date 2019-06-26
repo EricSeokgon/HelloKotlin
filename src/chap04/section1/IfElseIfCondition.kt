58c7a3279c2b5229fe1130e3e10d5c2b59a98185
hadesyi <leesk55@gmail.com>
2019-06-27 08:50:39 +0900

    in연산자와 범위 연산자로 조건식 간략하게 만들기

--- src/chap04/section1/IfElseIfCondition.kt
+++ src/chap04/section1/IfElseIfCondition.kt
@@ -7,9 +7,9 @@ fun main() {
 
     if (score >= 90.0) {
         grade = 'A'
-    } else if (score >= 80.0 && score <= 89.9) {
+    } else if (score in 80.0..89.9) {
         grade = 'B'
-    } else if (score >= 70.0 && score <= 79.9) {
+    } else if (score in 70.0..79.9) {
         grade = 'C'
     }
     println("Score: $score, Grade: $grade")
