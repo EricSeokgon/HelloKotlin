3d3dd17664e4a508431cfffa9b42385077e6ccfc
hadesyi <leesk55@gmail.com>
2019-06-27 12:58:09 +0900

    다양한 자료형의 인자 받기

---
+++ src/chap04/section1/WhenAnyCase.kt
@@ -0,0 +1,18 @@
+package chap04.section1
+
+fun main() {
+    cases("Hello") //2String형
+    cases(1) //Int형
+    cases(System.currentTimeMillis()) //현재 시간(밀리초 단위)을 Long형 값으로 반환
+    cases(MyClass()) //객체
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("Int: $obj")
+        "Hello" -> println("String: $obj")
+        is Long -> println("Long: $obj")
+        !is String -> println("Not a String")
+        else -> println("Unknown")
+    }
+}
