3f99627ad9fc4fbb5481c76611ea4603e54bc2e8
hadesyi <leesk55@gmail.com>
2019-06-29 07:22:41 +0900

    앵글 브래킷을 사용한 이름 중복 해결하기

--- src/chap05/section4/AngleBracketTest.kt
+++ src/chap05/section4/AngleBracketTest.kt
@@ -1,10 +1,29 @@
 package chap05.section4
 
 open class A {
-    open fun() = println("A Class f()")
+    open fun f() = println("A Class f()")
     fun a() = println("A class a()")
 }
 
-interface B{
+interface B {
+    fun f() = println("B Interface f()") //인터페이스는 기본적으 open임
+    fun b() = println("B Interface b()")
 
+}
+
+class C : A(), B { //쉼표(,)를 사용해 클래스와 인터페이스를 지정
+    //컴파일되려면 f()가 오버라이딩되어야 함
+    override fun f() = println("C Class f()")
+
+    fun test() {
+        f()
+        b()
+        super<A>.f()
+        super<B>.f()
+    }
+}
+
+fun main() {
+    val c = C()
+    c.test()
 }
