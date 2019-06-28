010bb8cd1580f70c3805edbfa5e02bb5733a6364
hadesyi <leesk55@gmail.com>
2019-06-28 12:37:33 +0900

    암묵적 라벨

--- src/chap04/section3/noinline/NoInlineLambdaReturn.kt
+++ src/chap04/section3/noinline/NoInlineLambdaReturn.kt
@@ -7,9 +7,9 @@ fun main() {
 fun retFunc() {
 
     println("start of retFunc")
-    inlineLambda(13, 3) lit@{ a, b -> //1람다식 블록의 시작 부분에 라벨을 지정
+    inlineLambda(13, 3) { a, b -> //1람다식 블록의 시작 부분에 라벨을 지정
         val result = a + b
-        if (result > 10) return@lit //2라벨을 사용한 블록의 끝부분으로 반환
+        if (result > 10) return@inlineLambda //2라벨을 사용한 블록의 끝부분으로 반환
         println("result: $result")
     } //3 이부분으로 빠져나감
     println("end of retFunc")
