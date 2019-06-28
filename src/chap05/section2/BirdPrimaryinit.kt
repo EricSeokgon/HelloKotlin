a1918a32177358be082a9a01d0df1233a506a2cf
hadesyi <leesk55@gmail.com>
2019-06-29 05:36:12 +0900

    프로퍼티의 기본값 지정

--- src/chap05/section2/BirdPrimaryinit.kt
+++ src/chap05/section2/BirdPrimaryinit.kt
@@ -1,8 +1,9 @@
 package chap05.section2.init
 
-class Bird(var name: String, var wing: Int, var beak: String, var color: String) {
+//프로퍼티의 기본값 지정
+class Bird(var name: String = "NONAME", var wing: Int = 2, var beak: String, var color: String) {
     // 초기화
-    init{
+    init {
         println("----------초기화 블록 시작----------")
         println("이름은 $name, 부리는 $beak")
         this.sing(3)
@@ -16,12 +17,11 @@ class Bird(var name: String, var wing: Int, var beak: String, var color: String)
 }
 
 fun main() {
-    val coco = Bird("mybird", 2, "short", "blue")
+    val coco = Bird(beak = "long", color = "blue") //기본값이 잇는 것은 생략하고 없는 것만 전달 가능
 
-    coco.color = "yellow"
-    println("coco.color: ${coco.color}")
-    coco.fly()
-    coco.sing(3)
+    println("coco.name: ${coco.name}, coco.wing ${coco.wing}")
+    println("coco.color: ${coco.color}, coco.beak ${coco.beak}")
+
 }
 
 
