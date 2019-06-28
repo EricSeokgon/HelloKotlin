5335154c178a5abdbee7b83941c85b98a779020a
hadesyi <leesk55@gmail.com>
2019-06-28 12:57:19 +0900

    break와 continue에 라벨 함께 사용하기

--- src/chap04/section3/NOrmalBreakContinue.kt
+++ src/chap04/section3/NOrmalBreakContinue.kt
@@ -4,7 +4,7 @@ fun main() {
     println("labelBreak")
     for (i in 1..5) {
         second@ for (j in 1..5) {
-            if (i == 3) break
+            if (j == 3) break
             print("i:$i, j:$j")
         }
         println("after for j")
