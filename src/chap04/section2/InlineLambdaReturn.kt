35b22a0777d0091fb43e0ca928a99b2c1fb059c8
hadesyi <leesk55@gmail.com>
2019-06-28 09:02:04 +0900

    inline을 사용한 람다식의 반환

---
+++ src/chap04/section2/InlineLambdaReturn.kt
@@ -0,0 +1,20 @@
+package chap04.section2
+
+fun main() {
+    retFunc()
+}
+
+fun retFunc() {
+
+    println("start of retFunc")
+    inlineLambda(13, 3) { a, b ->
+        val result = a + b
+        if (result > 10) return
+        println("result: $result")
+    }
+    println("end of retFunc")
+}
+
+inline fun inlineLambda(a: Int, b: Int, out: (Int, Int) -> Unit) {
+    out(a, b)
+}
