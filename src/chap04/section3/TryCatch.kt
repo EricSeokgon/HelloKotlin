849aeb4586402c3dee3aaa50cd8dab2d31493ddb
hadesyi <leesk55@gmail.com>
2019-06-28 13:12:58 +0900

    throw를 사용해 예외 발생시키디

--- src/chap04/section3/TryCatch.kt
+++ src/chap04/section3/TryCatch.kt
@@ -2,14 +2,14 @@ package chap04.section3
 
 fun main() {
     val a = 6
-    val b = 6
-    val c:Int
+    val b = 0
+    val c: Int
 
     try {
         c = a / b //0으로 나눔
-    } catch (e: ArithmeticException) {
-        println("Exception is handled. ${e.message}")
-    }finally {
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
         println("finally 블록은 반드시 항상 실행됨")
     }
 }
