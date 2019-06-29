a2ac4b56b506ede68b258050ab484509bb57f8f1
hadesyi <leesk55@gmail.com>
2019-06-30 04:18:19 +0900

    컴패니언 객체를 가진 코틀린의 클래스

---
+++ src/chap06/section3/KCustomer.kt
@@ -0,0 +1,10 @@
+package chap06.section3
+
+class KCustomer {
+    companion object {
+        const val LEVEL = "INTERMEDIATE"
+        @JvmStatic
+        fun login() = println("Login...")
+
+    }
+}
