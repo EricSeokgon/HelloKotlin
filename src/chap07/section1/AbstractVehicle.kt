75189a32a294c83bb05c6ea63e54291bffa12c94
hadesyi <leesk55@gmail.com>
2019-07-01 08:42:25 +0900

    추상 클래스 Vehicle 사용해 보기

---
+++ src/chap07/section1/AbstractVehicle.kt
@@ -0,0 +1,23 @@
+package chap07.section1
+
+//추상 클래스, 주생성자에는 비추상 프로퍼티 선언의 매개변수 3개가 있음
+abstract class Vehicle(val name: String, val color: String, val weight: Double) {
+    //추상 프로퍼티(반드시 하위 클래스에서 재저으이해 초기화해야 함)
+    abstract var maxSpeed: Double
+
+    //일반 프로러피(초깃값인 상태를 저장할 수 있음)
+    var year = "2018"
+
+    //추상 메서드(반드시 하위 클래스에서 구현해야 함)
+    abstract fun start()
+
+    abstract fun stop()
+
+    //일반 메서드
+    fun displaySpecs() {
+        println(
+            "Name: $name, Color:$color, Weight:$weight, Year:$year, Max Speed: $maxSpeed"
+        )
+    }
+
+}
