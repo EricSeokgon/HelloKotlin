6bf2ebb3a2681389af0d42bf6bd4a83f3548599c
hadesyi <leesk55@gmail.com>
2019-07-01 08:48:52 +0900

    게터를 구현한 프로퍼티

--- src/chap07/section1/InterfacePet.kt
+++ src/chap07/section1/InterfacePet.kt
@@ -2,8 +2,22 @@ package chap07.section1
 
 interface Pet {
     var category: String //abstract 키워드가 없어도 기본은 추상 프로퍼티
+    val msgTags: String //val 선언시 게터의 구현이 가능
+        get() = "I'm your lovely pet!"
+
     fun feeding() // 마찬가지로 추상 메서드
     fun patting() { //일반 메서드 : 구현부를 포함하면 일반적인 메서드가 기본이 됨
         println("Keep patting!") //구현부
     }
+}
+
+class Cat(override var category: String) : Pet {
+    override fun feeding() {
+        println("Feed the cat a tuna can!")
+    }
+}
+
+fun main() {
+    val obj = Cat("small")
+    println("Pet Message Tags: ${obj.msgTags}")
 }
