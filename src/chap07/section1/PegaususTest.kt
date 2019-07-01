8c9f8712335683b4b5396baabba67528a095ddd9
hadesyi <leesk55@gmail.com>
2019-07-01 09:01:14 +0900

    려어 인터페이스를 이용한 다중 상속

--- src/chap07/section1/PegaususTest.kt
+++ src/chap07/section1/PegaususTest.kt
@@ -15,3 +15,28 @@ interface Horse {
         println("jump!, max speed: $maxSpeed")
     }
 }
+
+class Pegasus : Bird, Horse {
+    override val wings: Int = 2
+    override val maxSpeed: Int = 100
+    override fun fly() {
+        println("Fly!")
+
+    }
+
+    override fun run() {
+        println("Run!")
+    }
+
+    override fun jump() {
+        super<Horse>.jump()
+        println("Pegasus Jump!")
+    }
+}
+
+fun main() {
+    val pegasus = Pegasus()
+    pegasus.fly()
+    pegasus.run()
+    pegasus.jump()
+}
