75f4a332b969a2a7159cd2cac385a732c7a5e96e
hadesyi <leesk55@gmail.com>
2019-07-01 12:51:40 +0900

    실드 클래스 작성하기

--- src/chap07/section2/SealedClass.kt
+++ src/chap07/section2/SealedClass.kt
@@ -2,9 +2,10 @@ package chap07.section2
 
 sealed class Result {
     open class Success(val message: String) : Result()
-    class Error(cal code: Int, val message: String) : Result()
+    class Error(val code: Int, val message: String) : Result()
 }
 
 class Status : Result() //실드 클래스 상속은 같은 파일에서만 가능
 class Inside : Result.Success("Status") //내부 클래스 상속
+
 
