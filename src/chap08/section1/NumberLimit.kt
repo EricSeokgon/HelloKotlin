74b83f48a0bec2525b62bf8642b5ee771d4a2238
hadesyi <leesk55@gmail.com>
2019-07-03 08:34:13 +0900

    자료형을 숫자형으로 제한하기

--- src/chap08/section1/NumberLimit.kt
+++ src/chap08/section1/NumberLimit.kt
@@ -1,3 +1,20 @@
 package chap08.section1
 
-자료
+class Calc<T : Number> {
+    fun plus(arg1: T, arg2: T): Double {
+        return arg1.toDouble() + arg2.toDouble()
+    }
+}
+
+fun main() {
+    val calc = Calc<Int>()
+    println(calc.plus(10, 20))
+
+    val calc2 = Calc<Double>()
+    val calc3 = Calc<Long>()
+    // val calc4 = Calc<String>()
+
+    println(calc2.plus(2.5, 3.5))
+    println(calc3.plus(5L, 10L))
+
+}
