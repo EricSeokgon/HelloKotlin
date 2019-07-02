845a4a5a85c1820449d7ea4124da09dcb3bd36b2
hadesyi <leesk55@gmail.com>
2019-07-02 12:53:30 +0900

    제네릭 클래스의 자료형 변환하기

--- src/chap08/section1/GenericCasting.kt
+++ src/chap08/section1/GenericCasting.kt
@@ -7,10 +7,10 @@ class Cup<T>
 
 fun main() {
     val obj1: Parent = Child()
-    val obj2: Child = Parent() // 오류! 자료형 불일치
+ //   val obj2: Child = Parent() // 오류! 자료형 불일치
 
-    val obj3: Cup<Parent> = Cup<Child>() // 오류! 자료형 불일치
-    val obj4: Cup<T> = Cup<Parent>() // 오류! 자료형 불일치
+   // val obj3: Cup<Parent> = Cup<Child>() // 오류! 자료형 불일치
+    //val obj4: Cup<T> = Cup<Parent>() // 오류! 자료형 불일치
 
     val obj5 = Cup<Child>()
     val obj6: Cup<Child> = obj5
