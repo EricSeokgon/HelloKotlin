10435260ad6957dbe8d5fe9a95e2c3b23c1dfc1f
hadesyi <leesk55@gmail.com>
2019-07-03 17:52:17 +0900

    불변형 list사용하기

--- src/chap09/section2/ListBasic.kt
+++ src/chap09/section2/ListBasic.kt
@@ -4,4 +4,10 @@ fun main() {
     var numbers: List<Int> = listOf(1, 2, 3, 4, 5)
     var names: List<String> = listOf("one", "two", "three")
 
+    for (name in names) {
+        println(name)
+    }
+    for (num in numbers) print(num)
+    println()
+
 }
