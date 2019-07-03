95a2058e563affac3fec8de9a0a54760a8b48b32
hadesyi <leesk55@gmail.com>
2019-07-03 12:43:53 +0900

    Any 자료형으로 지정된 배열 채우기

--- src/chap08/section2/ArrayAccessTest.kt
+++ src/chap08/section2/ArrayAccessTest.kt
@@ -14,7 +14,7 @@ fun main() {
     //세터에 의한 값의 설정
     arr.set(2, 7)
     arr[0] = 8
-    println("size: ${arr.size} arr[0]: ${arr[0]}, arr[2]: ${arr[2]} }")
+    println("size: ${arr.size} arr[0]: ${arr[0]}, arr[2]: ${arr[2]} ")
 
     //루프를 통한 배열 요소의 접근
     for (i in 0..arr.size - 1) {
