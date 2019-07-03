a39066abe47c40a398e890679765003621ce2717
hadesyi <leesk55@gmail.com>
2019-07-03 18:10:50 +0900

    TreeSet의 초기화

---
+++ src/chap09/section3/TreeSet.kt
@@ -0,0 +1,13 @@
+package chap09.section3
+
+import java.util.*
+
+fun main() {
+    val intsSortedSet: TreeSet<Int> = sortedSetOf(4, 1, 7, 2)
+    intsSortedSet.add(6)
+    intsSortedSet.remove(1)
+    println("intsSortedSet = ${intsSortedSet}")
+
+    intsSortedSet.clear()
+    println("intsSortedSet = ${intsSortedSet}")
+}
