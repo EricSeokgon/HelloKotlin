7657c3e827cd791f2b424c7005397a12ae18cb37
hadesyi <leesk55@gmail.com>
2019-07-04 08:58:45 +0900

    컬렉션의 매핑 확장 함수 사용하기

--- src/chap09/section4/ExtensionMapping.kt
+++ src/chap09/section4/ExtensionMapping.kt
@@ -4,4 +4,14 @@ fun main() {
     val list = listOf(1, 2, 3, 4, 5, 6)
     val listWithNUll = listOf(1, null, 3, null, 5, 6)
 
+    // map : 컬렉션에 주어진 식을 적용해 새로운 컬렉션 반환
+    println(list.map { it * 2 })
+
+    // mapIndexed: 컬렉션에 인덱스를 포함하고 주어진 식을 적용해 새로운 컬렉션 반환
+    val mapIndexed = list.mapIndexed({ index, it -> index * it })
+    println(mapIndexed)
+
+    // mapNotNull: null을 제외하고 식을 적용해 새로운 컬렉션 반환
+    println(listWithNUll.mapNotNull { it?.times(2) })
+
 }
