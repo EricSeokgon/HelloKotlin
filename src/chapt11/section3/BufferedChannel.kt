a5076031a48cc5ff9b25baada132e3a58542091e
hadesyi <leesk55@gmail.com>
2019-07-07 08:49:42 +0900

    버퍼를 가진 채널 구성하기.

---
+++ src/chapt11/section3/BufferedChannel.kt
@@ -0,0 +1,18 @@
+package chapt11.section3
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    val channel = Channel<Int>(3)
+    val sender = launch(coroutineContext) {
+        repeat(10) {
+            println("Sending $it")
+            channel.send(it)
+        }
+    }
+    kotlinx.coroutines.delay(1000)
+    sender.cancel()
+}
+
