528d01083ecc096edf583ce2ea336461508864ce
hadesyi <leesk55@gmail.com>
2019-07-07 08:38:10 +0900

    시작 시점 느춰보기

---
+++ src/chapt11/section2/AsyncStart.kt
@@ -0,0 +1,27 @@
+package chapt11.section2
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+suspend fun doWork1(): String {
+    delay(1000)
+    return "Work1"
+}
+
+suspend fun doWork2(): String {
+    delay(3000)
+    return "Work2"
+}
+
+fun main() = runBlocking {
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doWork1() }
+        val two = async(start = CoroutineStart.LAZY) { doWork2() }
+        println("AWAIT: ${one.await() + "_" + two.await()}")
+    }
+    println("Completed in $time ms")
+}
+
